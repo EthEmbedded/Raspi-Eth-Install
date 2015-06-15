e0dec6e8723605bd30ac788820d74d1ce4950784
phonikg <jgerryts@gmail.com>
2015-06-15 19:11:25 -0400

    Update eth-installer.sh

--- eth-installer.sh
+++ eth-installer.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 #First we need to perform some housekeeping & install dependencies
-apt-get -y update
-apt-get -y upgrade
-dpkg-reconfigure locales
-apt-get -y install raspi-copies-and-fills build-essential git scons cmake nodejs npm ntp cloud-utils golang libboost-all-dev automake unzip libgmp-dev libgmp3-dev libtool libleveldb-dev yasm libminiupnpc-dev libreadline-dev libncurses5-dev libcurl4-openssl-dev wget libjsoncpp-dev libargtable2-dev clang-3.5 lldb-3.5 libmicrohttpd-dev clang-3.5-doc clang-3.5-examples clang-format-3.5 clang-modernize-3.5 cpp11-migrate-3.5 libclang-3.5-dev libclang-common-3.5-dev libclang1-3.5 libclang1-3.5-dbg liblldb-3.5 liblldb-3.5-dev libllvm-3.5-ocaml-dev libllvm3.5 libllvm3.5-dbg lldb-3.5-dev llvm-3.5 llvm-3.5-dev llvm-3.5-doc llvm-3.5-examples llvm-3.5-runtime llvm-3.5-tools python-clang-3.5 python-lldb-3.5
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install raspi-copies-and-fills build-essential git scons cmake nodejs npm ntp cloud-utils golang libboost-all-dev automake unzip libgmp-dev libgmp3-dev libtool libleveldb-dev yasm libminiupnpc-dev libreadline-dev libncurses5-dev libcurl4-openssl-dev wget libjsoncpp-dev libargtable2-dev clang-3.5 lldb-3.5 libmicrohttpd-dev clang-3.5-doc clang-3.5-examples clang-format-3.5 clang-modernize-3.5 cpp11-migrate-3.5 libclang-3.5-dev libclang-common-3.5-dev libclang1-3.5 libclang1-3.5-dbg liblldb-3.5 liblldb-3.5-dev libllvm-3.5-ocaml-dev libllvm3.5 libllvm3.5-dbg lldb-3.5-dev llvm-3.5 llvm-3.5-dev llvm-3.5-doc llvm-3.5-examples llvm-3.5-runtime llvm-3.5-tools python-clang-3.5 python-lldb-3.5
 
 #We must compile cryptopp & json-rpc-cpp from source "the old way" for armhf
 
 #Cryptopp
 git clone https://github.com/mmoss/cryptopp.git
 cd cryptopp
-scons --shared --prefix=/usr
+sudo scons --shared --prefix=/usr
 cd ..
 
 #Json-rpc-cpp	
 git clone git://github.com/cinemast/libjson-rpc-cpp.git
 mkdir -p libjson-rpc-cpp/build
 cd libjson-rpc-cpp/build
-cmake .. 
-make
-make install
-ldconfig	
+sudo cmake .. 
+sudo make
+sudo make install
+sudo ldconfig	
 cd ../..
 
 #Now lets install Ethereum 
@@ -31,8 +30,8 @@ cd cpp-ethereum
 git checkout develop
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUNDLE=minimal
-make -j2
+sudo cmake .. -DCMAKE_BUILD_TYPE=Release -DBUNDLE=minimal
+sudo make -j2
 cd ../..
 
 #Finally we can run `geth` by going to the proper directory and executign the `./geth` command:
