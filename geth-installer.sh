2ebc04c5b93f3a300bd81461f79e823538ee4b09
phonikg <jgerryts@gmail.com>
2015-07-27 00:28:42 -0400

    Update geth-installer.sh

--- geth-installer.sh
+++ geth-installer.sh
@@ -1,37 +1,23 @@
 #!/bin/bash
 
-#First we need to perform some housekeeping & install dependencies
-echo -e "\e[32mFirst we need to perform some housekeeping & install dependencies\e[0m"
+#Upgrade Debian to "stretch"
+echo -e "\e[32mFirst we need to edit our sources.list to set stretch/testing as our source so we can install latest versions of dependencies\e[0m"
+sudo sed -i /deb/s/wheezy/stretch/g /etc/apt/sources.list
+sudo sed -i /deb/s/jessie/stretch/g /etc/apt/sources.list
 echo -e "\e[34mPerforming Housekeeping - Update...\e[0m"
 sudo apt-get -y update
 echo -e "\e[34mPerforming Housekeeping - Upgrade...\e[0m"
 sudo apt-get -y upgrade
+echo -e "\e[34mPerforming Debian Upgrade from `Jessie` to `Stretch`...\e[0m"
+sudo apt-get -y dist-upgrade 
+
+#Install dependencies
 echo -e "\e[34mInstalling Dependencies...\e[0m"
-sudo apt-get -y install raspi-copies-and-fills build-essential libgmp3-dev golang git
-
-#Install GO 1.4.2
-echo -e "\e[34mInstalling GO 1.4.2 from source...\e[0m"
-
-#Set environment variables:
-
-cd ~
-export PATH=$HOME/golang/bin:$PATH
-export GOPATH=$HOME/go
-mkdir -p $GOPATH
-
-#Install golang from source:
-
-cd ~
-git clone https://go.googlesource.com/go golang
-cd golang
-git checkout go1.4.2
-cd src
-sudo ./make.bash
+sudo apt-get -y install dphys-swapfile build-essential libgmp3-dev golang git raspi-copies-and-fills
 
 #Install go-ethereum OR `geth`
 echo -e "\e[34mInstalling go-ethereum...\e[0m"
 cd ~
 git clone https://github.com/ethereum/go-ethereum
 cd go-ethereum
-sudo make geth
-
+make geth
