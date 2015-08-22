706dec61049964dbe00ac00cc075a13e16555fdd
phonikg <jgerryts@gmail.com>
2015-08-22 16:37:18 -0400

    Update eth-installer.sh

--- eth-installer.sh
+++ eth-installer.sh
@@ -3,7 +3,15 @@
 #First we need to perform some housekeeping & install dependencies
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install raspi-copies-and-fills build-essential git python curl scons cmake libboost-all-dev automake unzip libgmp-dev libgmp3-dev libtool libleveldb-dev yasm libminiupnpc-dev libreadline-dev libncurses5-dev libcurl4-openssl-dev wget libjsoncpp-dev libargtable2-dev libmicrohttpd-dev
+sudo apt-get -y install raspi-copies-and-fills build-essential git python curl scons cmake libboost-all-dev automake unzip libgmp-dev libgmp3-dev libtool libleveldb-dev yasm libminiupnpc-dev libreadline-dev libncurses5-dev libcurl4-openssl-dev wget libjsoncpp-dev libargtable2-dev clang-3.5 lldb-3.5 libmicrohttpd-dev clang-3.5-doc clang-3.5-examples clang-format-3.5 clang-modernize-3.5 cpp11-migrate-3.5 libclang-3.5-dev libclang-common-3.5-dev libclang1-3.5 libclang1-3.5-dbg liblldb-3.5 liblldb-3.5-dev libllvm-3.5-ocaml-dev libllvm3.5 libllvm3.5-dbg lldb-3.5-dev llvm-3.5 llvm-3.5-dev llvm-3.5-doc llvm-3.5-examples llvm-3.5-runtime llvm-3.5-tools python-clang-3.5 python-lldb-3.5
+
+#Download armhf pre-compiled llvm-3.8 
+wget http://www.ethembedded.com/downloads/llvm-38-deb-files/llvm_deb_files.zip
+mkdir llvm-3.8
+cd llvm-3.8
+unzip llvm_deb_files.zip
+dpkg -i *.deb
+cd ..
 
 #We must compile cryptopp & json-rpc-cpp from source "the old way" for armhf
 
@@ -24,14 +32,13 @@ sudo ldconfig
 cd ../..
 
 #Now lets install Ethereum 
-#If this is your first time playing around with Ethereum, I would suggest starting with `geth`
 cd ~
 git clone https://github.com/ethereum/cpp-ethereum
 cd cpp-ethereum
 git checkout develop
 mkdir build
 cd build
-sudo cmake .. -DCMAKE_BUILD_TYPE=Release -DBUNDLE=minimal
-sudo make -j2
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUNDLE=minimal -DEVMJIT=off
+make -j2
 cd ../..
 
