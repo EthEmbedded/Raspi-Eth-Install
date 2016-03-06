c34e962143893ee9c3628485bc467bee4b1a5167
John Gerryts <jgerryts@gmail.com>
2016-03-06 16:30:08 -0500

    Update eth-installer.sh

--- eth-installer.sh
+++ eth-installer.sh
@@ -3,33 +3,13 @@
 #First we need to perform some housekeeping & install dependencies
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install build-essential git python curl scons cmake libboost-all-dev automake unzip libgmp-dev libgmp3-dev libtool libleveldb-dev yasm libminiupnpc-dev libreadline-dev libncurses5-dev libcurl4-openssl-dev wget libjsoncpp-dev libargtable2-dev libmicrohttpd-dev libedit-dev mesa-common-dev ocl-icd-libopencl1 opencl-headers libgoogle-perftools-dev ocl-icd-dev libv8-dev binfmt-support libffi-dev libobjc-4.9-dev libobjc4
-
-#Download armhf pre-compiled llvm-3.8 
-cd ~
-wget http://www.ethembedded.com/downloads/llvm-38-deb-files/llvm_deb_files.zip
-unzip llvm_deb_files.zip
-cd llvm_deb_files
-sudo dpkg -i *.deb
-cd ..
-
-#We must compile cryptopp & json-rpc-cpp from source for armhf
+sudo apt-get -y install build-essential git python curl scons cmake libboost-all-dev automake unzip libgmp-dev libgmp3-dev libtool libleveldb-dev yasm libminiupnpc-dev libreadline-dev libncurses5-dev libcurl4-openssl-dev wget libjsoncpp-dev libargtable2-dev libmicrohttpd-dev libedit-dev mesa-common-dev ocl-icd-libopencl1 opencl-headers libgoogle-perftools-dev ocl-icd-dev libv8-dev binfmt-support libffi-dev libobjc-4.9-dev libobjc4 libjsonrpccpp-dev libjsonrpccpp-tools
 
 #Cryptopp
 git clone https://github.com/mmoss/cryptopp.git
 cd cryptopp
 sudo scons --shared --prefix=/usr
 cd ..
-
-#Json-rpc-cpp	
-git clone git://github.com/cinemast/libjson-rpc-cpp.git
-mkdir -p libjson-rpc-cpp/build
-cd libjson-rpc-cpp/build
-sudo cmake .. 
-sudo make
-sudo make install
-sudo ldconfig	
-cd ../..
 
 #Now lets install Ethereum 
 cd ~
